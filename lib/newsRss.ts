f111a2d83c7adbde9c62fb0415a6db9861de5dcb
fsgoldcfu <fsgoldcfu@gmail.com>
2026-06-28 06:58:57 +0800

    Delete lib/newsRss.ts

--- lib/newsRss.ts
+++
@@ -1,8 +0,0 @@
-// Placeholder for news RSS fetching
-// This will be implemented to fetch HK stock news from various sources
-
-export async function getHKStockNews(symbol: string): Promise<any[]> {
-  // TODO: Implement RSS/news API integration
-  // For now, return empty array
-  return [];
-}
